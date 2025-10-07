ded24f77fd9e851357a5711f06a2806f28d68c41
John Farrell <johnfarrell@fastmail.com>
2025-10-07 20:56:23 +0100

    create the footer with js

---
+++ pages/src/components/create_footer.ts
@@ -0,0 +1,27 @@
+export function create_footer() {
+  const text = document.createElement("p");
+  text.innerText = "Created by John Farrell (2025)";
+
+  const link = document.createElement("a");
+  link.href = "https://www.johnfarrell.dev/";
+  link.target = "_blank";
+  link.referrerPolicy = "no-referrer";
+  link.rel = "noreferrer";
+  link.text = "JohnFarrell.dev";
+
+  const container = document.createElement("div");
+  container.className = "container";
+
+  container.appendChild(text);
+  container.appendChild(link);
+
+  const hr = document.createElement("hr");
+
+  const footer = document.createElement("footer");
+  footer.appendChild(hr);
+  footer.appendChild(container);
+
+  console.log(footer);
+
+  document.querySelector("#app")?.appendChild(footer);
+}
